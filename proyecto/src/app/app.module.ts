7e10fd6f444fe6e083d95e13fe535c974ec4cef8
Edwin Escobar <edwin.escobar@grupoboticario.com.co>
2023-08-28 21:16:41 -0500

    Backlog: Modulo dashboard #8 - 0001

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NgIf } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {NgFor} from '@angular/common';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +23,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatSidenavModule,
+    NgIf,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatGridListModule,
+    NgFor
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
