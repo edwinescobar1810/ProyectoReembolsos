76271fd5b2f1ca3c1e6001b600692a044065fb32
Nicolas Alfonso Hernandez Franco <Nicolas.Hernandez@grupoboticario.com.co>
2023-09-13 19:23:27 -0500

    Versión con formaulario de reembolsos, ajuste en iconos y tarjeta de perfil

--- proyecto/src/app/reembolsos/reembolsos.component.ts
+++ proyecto/src/app/reembolsos/reembolsos.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { OpenMiperfilComponent } from '../open-miperfil/open-miperfil.component';
+
 import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-reembolsos',
   templateUrl: './reembolsos.component.html',
-  styleUrls: ['./reembolsos.component.css']
+  styleUrls: ['./reembolsos.component.css'],
 })
 export class ReembolsosComponent {
 
